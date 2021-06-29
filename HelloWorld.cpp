3ae661ad362ee65dc7b08ad86788f3177618f9b5
nik2808 <madhogarianikunj@gmail.com>
2021-06-29 11:56:36 +0530

    First cpp code.

---
+++ HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    cout<<"Hello World!\n";
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ll t;cin >> t;while(t--) 
+        solve();
+}
